08dc0325c7198c8cb41ebd1b59d14bc5d564aaaa
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-04-11 10:38:06 -0400

    Working on layout for main page

--- global.js
+++ global.js
@@ -1,3 +1,4 @@
+//#region Navbar
 const NAV = document.getElementById('nav')
 let navShown = false;
 
@@ -11,3 +12,28 @@ function toggleNav()
     }
     NAV.classList.add('hidden')
 }
+//#endregion
+//#region screen-100
+const mainContainers = document.getElementsByClassName('screen-100')
+for (let i = 0; i < mainContainers.length; ++i)
+{
+    mainContainers[i].style.height = '100svh'
+}
+onResize();
+
+window.addEventListener('resize', function() {onResize();})
+
+function onResize() {
+    for (let i = 0; i < mainContainers.length; ++i)
+    {
+        mainContainers[i].style.height = '100svh' 
+
+        if (isOverflown(mainContainers[i])) {
+            mainContainers[i].style.height = ''
+        }
+    }
+}
+function isOverflown(element) {
+    return element.scrollHeight > element.clientHeight;
+  }
+//#endregion
