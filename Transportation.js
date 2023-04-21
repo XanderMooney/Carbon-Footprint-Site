e9040caae0e70aa47d0c0b7a42ee25725bb51fc6
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-04-21 08:07:15 -0400

    Transportation page

---
+++ Transportation.js
@@ -0,0 +1,23 @@
+// let imgNum = 23;
+// let carImgs;
+
+// collectCars()
+
+
+// function collectCars()
+// {
+//     let availableNums;
+//     let index = 0;
+//     for (let i = 0; i < imgNum; ++i)
+//     {
+//         availableNums[i] = i;
+//     }
+//     for (let i = 0; i < 20; ++i)
+//     {
+//         while (!availableNums.contains(index))
+//         {
+//             let index = Math.random() * (imgNum - 1) + imgNum
+//         }
+//         carImgs[i] = "imgs/Transportation/" + index
+//     }
+// }
